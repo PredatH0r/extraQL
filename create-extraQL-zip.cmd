a1bbccb35894695d2adbedab174b028ed5595bd5
hbeham <horst@beham.biz>
2014-07-19 19:03:05 +0200

    - removed "new alt browser" script
    - removed source code from release .zip file

--- create-extraQL-zip.cmd
+++ create-extraQL-zip.cmd
@@ -9,15 +9,6 @@ if exist source.zip del source.zip
 %zipper% a -tzip extraQL.zip "%abs%\bin\Debug\extraQL.exe" images scripts
 if errorlevel 1 goto error
 
-rem package source
-%zipper% a -tzip source.zip * -x!*.zip -x!*.user -x!bin -x!obj -x!data
-if errorlevel 1 goto error
-
-rem add source package to binary package
-%zipper% a -tzip extraQL.zip source.zip
-if errorlevel 1 goto error
-del source.zip
-
 :success
 echo.
 echo SUCCESS !!!
