e1377e49aeee87ba8f2b07014fe2f8ac09ffa48b
hbeham <horst@beham.biz>
2014-08-07 23:45:26 +0200

    added script to set start page and load it when starting QL

---
+++ scripts/startpage.usr.js
@@ -0,0 +1,26 @@
+// ==UserScript==
+// @id           startpage
+// @name         Start Page
+// @description  Opens QL with your preferred start page. Set any page as your start page with "Userscripts / Set Start Page"
+// @author       PredatH0r
+// ==/UserScript==
+
+(function () {
+  function init() {
+    var home = quakelive.cvars.Get("web_home");
+    if (home && home.value)
+      window.location.href = "/#!" + home.value;
+
+    HOOK_MANAGER.addMenuItem("Set as Start Page", setStartPage);
+  }
+
+  function setStartPage() {
+    var page = window.location.hash;
+    if (page.length > 2) {
+      page = page.substr(2);
+      quakelive.cvars.Set("web_home", page);
+    }
+  }
+
+  init();
+})();
