4719b0a086410814187f2f345519ac6b8033bc8c
hbeham <horst@beham.biz>
2014-08-11 02:07:57 +0200

    removed accidentally committed file "foo.usr.js"

--- scripts/foo.usr.js
+++
@@ -1,8 +0,0 @@
-// ==UserScript==
-// @id          foo
-// @name        test
-// @version     1.0
-// @author      PredatH0r
-// @description	Test for unofficial script files on local HDD
-// @unwrap
-// ==/UserScript==
