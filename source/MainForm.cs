fe02fca251a076c3bad5395adba00fa5c1f16147
hbeham <horst@beham.biz>
2015-10-30 14:14:41 +0100

    - added code trying to track down issues why some people cannot initialze steam_api.dll

--- source/MainForm.cs
+++ source/MainForm.cs
@@ -11,7 +11,7 @@ namespace ExtraQL
 {
   public partial class MainForm : Form
   {
-    public const string Version = "2.3";
+    public const string Version = "2.4beta1";
 
     private readonly Config config;
     private readonly HttpServer server;
